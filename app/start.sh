28b9460090bc957981a7cf90cf0dc3a84f0ae818
ColletSimon <simoncollet2005@gmail.com>
2025-04-17 15:32:01 -0400

    ajout de commentaires et modification de l'installation de ansible

--- app/start.sh
+++ app/start.sh
@@ -1,6 +1,29 @@
 apt update
+echo installation de python
 apt install -y python3-pip
-apt install -y qemu-kvm libvirt-bin virtinst virt-viewer libguestfs-tools virt-manager uuid-runtime
+
+echo installation de qemu-kvm
+apt install -y qemu-kvm 
+
+echo installation de virtinst
+apt install -y virtinst 
+
+echo installation de virt-viewer
+apt install -y virt-viewer 
+
+echo installation de libguestfs-tools
+apt install -y libguestfs-tools 
+
+echo installation de virt-manager
+apt install -y virt-manager 
+
+echo installation de uuid-runtime
+apt install -y uuid-runtime
+
+echo installation de ansible
+apt install -y ansible
+
+echo installation de requirements.txt
 pip install --no-cache-dir -r requirements.txt
 
 echo installation des dépendances fait !
