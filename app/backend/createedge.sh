5662ad06d970b6291b093d4ecdc52fe52af3b096
ColletSimon <simoncollet2005@gmail.com>
2025-04-17 11:41:57 -0400

    modif nom bridge

--- app/backend/createedge.sh
+++ app/backend/createedge.sh
@@ -1,1 +1,1 @@
- sudo virt-install --name $1 --os-variant ubuntu20.04 --vcpus 2 --ram $2 --location http://ftp.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/ --network bridge=virbr0,model=virtio --graphics none --extra-args='console=ttyS0,115200n8 serial'
+ sudo virt-install --name $1 --os-variant ubuntu20.04 --vcpus 2 --ram $2 --location http://ftp.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/ --network bridge=virbr10,model=virtio --graphics none --extra-args='console=ttyS0,115200n8 serial'
