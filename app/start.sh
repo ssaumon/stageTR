a2b5b42ae7b04167a4fab7c7b6c77ee2fe3568d4
ColletSimon <simoncollet2005@gmail.com>
2025-04-17 11:12:13 -0400

    automatisation de la création d'une vm (ici edge)

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,12 @@
 apt update
 apt install -y python3-pip
+apt install -y qemu-kvm libvirt-bin virtinst virt-viewer libguestfs-tools virt-manager uuid-runtime
 pip install --no-cache-dir -r requirements.txt
 
 echo installation des dépendances fait !
+
+virsh define backend/net.xml
+
+echo création du commutateur virtuel
 
 python3 app.py
