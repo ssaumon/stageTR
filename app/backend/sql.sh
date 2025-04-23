bf2e14fb42c5d89427338f01f3caf58aa2f5bb07
ColletSimon <simoncollet2005@gmail.com>
2025-04-23 10:04:38 -0400

    selection de la bdd

--- app/backend/sql.sh
+++ app/backend/sql.sh
@@ -1,5 +1,5 @@
 mysql -u root --execute='CREATE DATABASE IF NOT EXISTS BDD_VMs;'
 mysql -u root --execute='USE BDD_VMs;'
-mysql -u root --execute='CREATE TABLE IF NOT EXISTS edge (nom varchar(255) PRIMARY KEY ,cpu INT NOT NULL,ram INT NOT NULL, statut varchar(255));'
-mysql -u root --execute='CREATE TABLE IF NOT EXISTS iot (nom varchar(255) PRIMARY KEY ,cpu INT NOT NULL,ram INT NOT NULL, statut varchar(255));'
-mysql -u root --execute='CREATE TABLE IF NOT EXISTS applications (nom varchar(255) PRIMARY KEY , manifest TEXT(65535));'
+mysql -u root --execute='USE BDD_VMs; CREATE TABLE IF NOT EXISTS edge (nom varchar(255) PRIMARY KEY ,cpu INT NOT NULL,ram INT NOT NULL, statut varchar(255));'
+mysql -u root --execute='USE BDD_VMs; CREATE TABLE IF NOT EXISTS iot (nom varchar(255) PRIMARY KEY ,cpu INT NOT NULL,ram INT NOT NULL, statut varchar(255));'
+mysql -u root --execute='USE BDD_VMs; CREATE TABLE IF NOT EXISTS applications (nom varchar(255) PRIMARY KEY , manifest TEXT(65535));'
