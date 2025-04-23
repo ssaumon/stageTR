7b3d4bc701b339ec9f3317d9e0dc6b76db458090
ColletSimon <simoncollet2005@gmail.com>
2025-04-23 10:53:06 -0400

    changement de quotes

--- app/backend/sql.sh
+++ app/backend/sql.sh
@@ -3,6 +3,6 @@ mysql -u root --execute='USE BDD_VMs;'
 mysql -u root --execute='USE BDD_VMs; CREATE TABLE IF NOT EXISTS edge (nom varchar(255) PRIMARY KEY ,cpu INT NOT NULL,ram INT NOT NULL, statut varchar(255));'
 mysql -u root --execute='USE BDD_VMs; CREATE TABLE IF NOT EXISTS iot (nom varchar(255) PRIMARY KEY ,cpu INT NOT NULL,ram INT NOT NULL, statut varchar(255));'
 mysql -u root --execute='USE BDD_VMs; CREATE TABLE IF NOT EXISTS applications (nom varchar(255) PRIMARY KEY , manifest TEXT(65535));'
-mysql -u root --execute='ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'bonjour';
+mysql -u root --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'bonjour';
 FLUSH PRIVILEGES;
-'
+"
