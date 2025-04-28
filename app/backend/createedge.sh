cee5186eb9ba21f39e71ef4b6a0109b3593881d1
ColletSimon <simoncollet2005@gmail.com>
2025-04-28 16:49:59 -0400

    creation du dossier

--- app/backend/createedge.sh
+++ app/backend/createedge.sh
@@ -2,6 +2,11 @@
 # sudo virt-install --name $1 --cloud-init cloud-init.yml --os-variant ubuntu20.04 --vcpus $3 --ram $2 --location http://ftp.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/ --network bridge=virbr10,model=virtio --graphics none --disk path=/mnt/vms/$1.qcow2,size=20,bus=virtio,format=qcow2 --console pty,target_type=serial --extra-args='auto=true priority=critical console=ttyS0,115200n8 serial'
 
 # sudo virt-install   --name $1   --memory $2   --vcpus $3   --cloud-init user-data=backend/cloudinit/user-data  --disk path=/home/jammy-server-cloudimg-amd64.img,format=raw   --os-type linux   --os-variant detect=on   --import   --network bridge=virbr10  --graphics none
+
+if [! -d "cloudinit/user-data" ]; then
+    mkdir cloudinit/user-data
+fi
+
 touch cloudinit/user-data/$1
 cat --show-tabs clondinit/user-data | sed "s/{{hostname}}/$1/g" > cloudinit/user-data/$1
 
