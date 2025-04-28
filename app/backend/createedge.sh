b443a6fde4cf34a2420740a0626dbac7393bb870
ColletSimon <simoncollet2005@gmail.com>
2025-04-28 16:58:10 -0400

    modif chemin

--- app/backend/createedge.sh
+++ app/backend/createedge.sh
@@ -4,10 +4,10 @@
 # sudo virt-install   --name $1   --memory $2   --vcpus $3   --cloud-init user-data=backend/cloudinit/user-data  --disk path=/home/jammy-server-cloudimg-amd64.img,format=raw   --os-type linux   --os-variant detect=on   --import   --network bridge=virbr10  --graphics none
 
 if [ ! -d "cloudinit/user-data" ]; then
-    mkdir cloudinit/user-data
+    mkdir backend/cloudinit/user-data
 fi
 
-touch cloudinit/user-data/$1
-cat --show-tabs clondinit/user-data | sed "s/{{hostname}}/$1/g" > cloudinit/user-data/$1
+touch backend/cloudinit/user-data/$1
+cat --show-tabs backend/clondinit/user-data | sed "s/{{hostname}}/$1/g" > backend/cloudinit/user-data/$1
 
 sudo virt-install --name $1 --os-variant detect=on,name=ubuntujammy --memory $2 --vcpus $3 --network bridge=virbr10,model=virtio --graphics none --disk path=/mnt/vms/$1.qcow2,size=20,bus=virtio,format=qcow2,backing_store="/home/jammy-server-cloudimg-amd64.img" --cloud-init user-data=cloudinit/user-data/$1 --import
