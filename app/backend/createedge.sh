fccffc0ea452227700b2b8d1271adcbc3f36cc11
ColletSimon <simoncollet2005@gmail.com>
2025-04-28 16:29:40 -0400

    hostname en focntion du nom de la vm

--- app/backend/createedge.sh
+++ app/backend/createedge.sh
@@ -3,4 +3,6 @@
 
 # sudo virt-install   --name $1   --memory $2   --vcpus $3   --cloud-init user-data=backend/cloudinit/user-data  --disk path=/home/jammy-server-cloudimg-amd64.img,format=raw   --os-type linux   --os-variant detect=on   --import   --network bridge=virbr10  --graphics none
 
-sudo virt-install --name $1 --os-variant detect=on,name=ubuntujammy --memory $2 --vcpus $3 --network bridge=virbr10,model=virtio --graphics none --disk path=/mnt/vms/$1.qcow2,size=20,bus=virtio,format=qcow2,backing_store="/home/jammy-server-cloudimg-amd64.img" --disk path=/var/lib/libvirt/images/seed.iso,device=cdrom --import
+cat --show-tabs user-data | sed "s/{{hostname}}/$1/g" >> cloudinit/user-data/$1
+
+sudo virt-install --name $1 --os-variant detect=on,name=ubuntujammy --memory $2 --vcpus $3 --network bridge=virbr10,model=virtio --graphics none --disk path=/mnt/vms/$1.qcow2,size=20,bus=virtio,format=qcow2,backing_store="/home/jammy-server-cloudimg-amd64.img" --cloud-init user-data=cloudinit/user-data/$1 --import
