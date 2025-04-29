7f7a1d1baa9f4a53c52e66367e55def4a142329e
ColletSimon <simoncollet2005@gmail.com>
2025-04-29 10:04:01 -0400

    ajout de l'entete dans deleteVM

--- app/backend/deleteVM.sh
+++ app/backend/deleteVM.sh
@@ -1,4 +1,5 @@
-virsh destroy $1
-virsh undefine $1
-rm backend/cloudinit/user-data.d/$1
-rm backend/cloudinit/meta-data.d/$1
+#!/bin/bash
+ virsh destroy $1
+ virsh undefine $1
+ rm backend/cloudinit/user-data.d/$1
+ rm backend/cloudinit/meta-data.d/$1
