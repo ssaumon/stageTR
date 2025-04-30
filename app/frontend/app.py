d04edda09c17fc330acb83d7c16b8945be982fbd
ColletSimon <simoncollet2005@gmail.com>
2025-04-30 14:53:20 -0400

    test si une vm s'appelle deja comme ca

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -38,10 +38,26 @@ def create_edge():
     if "nom" in data.keys() and "ram" in data.keys():
         #r=requests.post(f"{backip}:5000/createedge",data=data)
         nom,ram,cpu=data["nom"],data["ram"],data["cpu"]
-        cur.execute("INSERT INTO edge VALUES (%s, %s,%s,'en création');", (nom,cpu,ram))
-        cnx.commit()
-        subprocess.Popen(["./backend/createedge.sh", nom, ram, cpu])
-    return render_template("index.j2")
+        cur.execute("SELECT nom from iot;")
+        li=[n[0] for n in cur.fetchall()]
+        cur.execute("SELECT nom from edge;")
+        li.extend([n[0] for n in cur.fetchall()])
+        print(li)
+        if nom in li:
+            err="le nom "+nom+" est déjà utilisé"
+        elif re.search(r"\s",nom):
+            err="Il y a un espace dans : "+nom
+        elif int(ram)<2048:
+            err="RAM insuffisante : "+ram+" < 2048"
+        elif int(cpu)<2:
+            err="CPUs insuffisants : "+cpu+" < 2"
+        else:
+            cur.execute("INSERT INTO edge VALUES (%s, %s,%s,'en création');", (nom,cpu,ram))
+            cnx.commit()
+            subprocess.Popen(["./backend/createedge.sh", nom, ram, cpu])
+        cur.execute("SELECT * from edge;")
+        vms=cur.fetchall()
+    return render_template("edge.j2",vms=vms, err=err)
 
 
 @app.route("/createiot", methods=["POST"])
@@ -50,8 +66,14 @@ def create_iot():
     data = request.form.to_dict()
     if "nom" in data.keys() and "ram" in data.keys() and "cpu" in data.keys():
         nom,ram,cpu=data["nom"],data["ram"],data["cpu"]
-        if re.search(r"\s",nom):
-            print("oui")
+        cur.execute("SELECT nom from iot;")
+        li=[n[0] for n in cur.fetchall()]
+        cur.execute("SELECT nom from edge;")
+        li.extend([n[0] for n in cur.fetchall()])
+        print(li)
+        if nom in li:
+            err="le nom "+nom+" est déjà utilisé"
+        elif re.search(r"\s",nom):
             err="Il y a un espace dans : "+nom
         elif int(ram)<512:
             err="RAM insuffisante : "+ram+" < 512"
@@ -74,8 +96,6 @@ def deledge():
     data = request.form.to_dict()
     if "nom" in data.keys():
         nom=data["nom"]
-        print(nom)
-        print(Path.cwd())
         cur.execute("DELETE FROM edge WHERE nom = %s;", (nom,))
         cnx.commit()
         subprocess.Popen(["./backend/deleteVM.sh", nom])
