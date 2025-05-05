d755cb23acb64d2012d54f0393fca2aff3fbf65c
ColletSimon <simoncollet2005@gmail.com>
2025-05-05 10:37:41 -0400

    probleme lors de la suppression d'une vm

--- app/backend/deliphost.sh
+++ app/backend/deliphost.sh
@@ -1,13 +1,16 @@
 hosts=$(cat /etc/hosts)
-echo "" > /etc/hosts
+echo "" > /etc/hosts.tmp
 while read -r li
 do
     echo $li
-    if [[ "$li" =~ '192.*' ]]; then
+    if [[ "$li" =~ "192.*" ]]; then
         if [[ ! "$li" =~ " $1 " ]]; then
-            echo $li >> /etc/hosts
+            echo $li >> /etc/hosts.tmp
         fi
     else
-        echo $li >> /etc/hosts
+        echo $li >> /etc/hosts.tmp
     fi
 done < "/etc/hosts"
+
+rm /etc/hosts
+mv /etc/hosts.tmp /etc/hosts
