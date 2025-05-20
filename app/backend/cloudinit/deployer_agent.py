dcb64b110953e04afedf080e7cfd86b00deeeb9d
ColletSimon <simoncollet2005@gmail.com>
2025-05-20 16:10:48 -0400

    suppression des () dans request.data

--- app/backend/cloudinit/deployer_agent.py
+++ app/backend/cloudinit/deployer_agent.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route("/create")
 def create():
-    data=json.load(request.data())
+    data=json.load(request.data)
     nom,manifest = data["nom"],data["manifest"]
     if (not Path.is_file(f"/var/lib/rancher/k3s/server/manifests/shared/{nom}")):
         subprocess.run(["touch",f"/var/lib/rancher/k3s/server/manifests/shared/{nom}"])
@@ -20,13 +20,13 @@ def create():
     return 200
 @app.route("/delete")
 def delete():
-    data=json.load(request.data())
+    data=json.load(request.data)
     nom = data["nom"]
     subprocess.run(["rm", f"/var/lib/rancher/k3s/server/manifests/shared/{nom}"])
     return 200
 @app.route("/update")
 def update():
-    data=json.load(request.data())
+    data=json.load(request.data)
     nom,manifest = data["nom"],data["manifest"]
     if (Path.is_file(f"/var/lib/rancher/k3s/server/manifests/shared/{nom}")):
         subprocess.run(["echo",f"{manifest} > /var/lib/rancher/k3s/server/manifests/shared/{nom}"])
