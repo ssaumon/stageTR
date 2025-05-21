9a49988ed5a406879e3296a5df83b07feac9b3a3
ColletSimon <simoncollet2005@gmail.com>
2025-05-21 11:03:38 -0400

    service deployer-agent

--- app/backend/cloudinit/deployer_agent.py
+++ app/backend/cloudinit/deployer_agent.py
@@ -17,19 +17,19 @@ def create():
     if (not Path.is_file(f"/var/lib/rancher/k3s/server/manifests/shared/{nom}")):
         subprocess.run(["touch",f"/var/lib/rancher/k3s/server/manifests/shared/{nom}"])
         subprocess.run(["echo",f"{manifest} > /var/lib/rancher/k3s/server/manifests/shared/{nom}"])
-    return "", 200
+    return "valide", 200
 @app.route("/delete")
 def delete():
     data=json.load(request.data)
     nom = data["nom"]
     subprocess.run(["rm", f"/var/lib/rancher/k3s/server/manifests/shared/{nom}"])
-    return "", 200
+    return "oui", 200
 @app.route("/update")
 def update():
     data=json.load(request.data)
     nom,manifest = data["nom"],data["manifest"]
     if (Path.is_file(f"/var/lib/rancher/k3s/server/manifests/shared/{nom}")):
         subprocess.run(["echo",f"{manifest} > /var/lib/rancher/k3s/server/manifests/shared/{nom}"])
-    
+    return "oui", 200
 
 app.run(host="0.0.0.0", port=5001)
