c5c75757823a8861c9af2be2d349754cc711fd1e
ColletSimon <simoncollet2005@gmail.com>
2025-05-21 16:15:07 -0400

    with open pour modifier le fichiers manifests

--- app/backend/cloudinit/deployer_agent.py
+++ app/backend/cloudinit/deployer_agent.py
@@ -17,7 +17,9 @@ def create():
     nom,manifest = data["nom"],data["manifest"]
     if (not Path(f"/var/lib/rancher/k3s/server/manifests/shared/{nom}").is_file()):
         subprocess.run(["touch",f"/var/lib/rancher/k3s/server/manifests/shared/{nom}"])
-        subprocess.run(["echo",f"{manifest} > /var/lib/rancher/k3s/server/manifests/shared/{nom}"])
+        with open(f"/var/lib/rancher/k3s/server/manifests/shared/{nom}", 'w') as f:
+            for row in manifest:
+                f.writelines(manifest)
     return "valide", 200
 @app.route("/delete", methods=["POST"])
 def delete():
